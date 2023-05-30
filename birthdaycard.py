ff0af2194b99feeb34f99e1274153baad0a10ca0
gabcqw <134643615+gabcqw@users.noreply.github.com>
2023-05-30 20:30:56 +0100

    Add files via upload

---
+++ birthdaycard.py
@@ -0,0 +1,16 @@
+print("Good day! I am RobA. Nice meeting you!")
+
+print("What's your name?")
+name = input("Tell me your name: ")
+print("What year were you born in?")
+year = int(input("I was born in: "))
+year_sum= 2023 - year
+
+print(f"Hey {name}, let's celebrate your {year_sum} years of awareness!")
+print(f"Wishing you a day filled with joy and laughter as you turn {year_sum}!")
+
+# grinning squinting face
+print("\U0001F606")
+
+print(f"With love and best wishes,")
+print(f"Gabby")
